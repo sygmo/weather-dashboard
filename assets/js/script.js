16efe2f11c2c7f7dc82e412a59e9f57ba4fb10f1
Sydney Mercier <sydney.mercier91@gmail.com>
2022-01-16 13:13:06 -0600

    adjusted styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -130,6 +130,7 @@ function getOneCallApi(lat, lon, cityName) {
             var dailyForecast = data.daily;
             for (var i = 0; i < 5; i++) {
                 var dayEl = document.createElement('div');
+                dayEl.className = 'forecast-day text-white';
 
                 var dateEl = document.createElement('h4');
                 dateEl.textContent = moment.unix(dailyForecast[i].dt).format("M/D/YYYY");
@@ -162,14 +163,3 @@ searchHistoryEl.addEventListener('click', function (event) {
         getApiFromHistory(event.target.textContent);
     }
 })
-
-
-
-
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
